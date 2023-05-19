ab4b7cca46c5d05b9b1b1b4056a944d196065294
okey_google <112944671+pollyxashs@users.noreply.github.com>
2023-05-19 17:50:20 +0300

    Add files via upload

---
+++ lab7/Circular_Buffer.h
@@ -0,0 +1,237 @@
+#pragma once
+#include <iostream>
+#include <iterator>
+
+template<typename Buff, typename Tp, typename Distance = ptrdiff_t, typename Pointer = Tp*, typename Reference = Tp&>
+class CircularIterator : public std::iterator<std::random_access_iterator_tag, Tp> {
+private:
+    Buff* buff;
+    int index_;
+
+public:
+    typedef typename std::iterator<std::random_access_iterator_tag, Tp, Distance, Pointer, Reference> base_iterator;
+
+    typedef typename std::random_access_iterator_tag ;
+    typedef typename base_iterator::value_type;
+    typedef typename base_iterator::difference_type ;
+    typedef typename base_iterator::pointer;
+    typedef typename base_iterator::reference ;
+
+    CircularIterator(const CircularIterator&) = default;
+    ~CircularIterator() = default;
+    CircularIterator(Buff& buff, int index) : buff(&buff), index_(index) {}
+
+    CircularIterator& operator=(const CircularIterator& it) {
+        buff = it.buff;
+        index_ = it.index_;
+        return *this;
+    }
+
+    Tp& operator*() {
+        return (*buff)[index_];
+    }
+
+    Tp* operator->() {
+        return &(*buff)[index_];
+    }
+
+    friend bool operator==(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ == it2.index_;
+    }
+
+    friend bool operator!=(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ != it2.index_;
+    }
+
+    friend bool operator>=(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ >= it2.index_;
+    }
+
+    friend bool operator>(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ > it2.index_;
+    }
+
+    friend bool operator<=(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ <= it2.index_;
+    }
+
+    friend bool operator<(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ < it2.index_;
+    }
+
+    CircularIterator operator++() {
+        ++index_;
+        return *this;
+    }
+
+    CircularIterator operator++(int) {
+        CircularIterator temp = *this;
+        ++index_;
+        return temp;
+    }
+
+    CircularIterator operator--() {
+        --index_;
+        return *this;
+    }
+
+    CircularIterator operator--(int) {
+        CircularIterator temp = *this;
+        --index_;
+        return temp;
+    }
+
+    CircularIterator operator+=(const int n) {
+        index_ += n;
+        return *this;
+    }
+
+    CircularIterator operator-=(const int n) {
+        index_ -= n;
+        return *this;
+    }
+
+    friend CircularIterator operator+(const CircularIterator& it, const int n) {
+        CircularIterator result = it;
+        result += n;
+        return result;
+    }
+
+    friend CircularIterator operator+(const int n, const CircularIterator& it) {
+        return it + n;
+    }
+
+    friend CircularIterator operator-(const CircularIterator& it, const int n) {
+        CircularIterator result = it;
+        result -= n;
+        return result;
+    }
+
+    friend int operator-(const CircularIterator& it1, const CircularIterator& it2) {
+        return it1.index_ - it2.index_;
+    }
+};
+
+template <typename T>
+class CircularBuffer {
+private:
+    int capacity_;
+    int size_;
+    T* begin_;
+    int num_;
+
+public:
+    CircularBuffer() {
+        capacity_ = 0;
+        size_ = 0;
+        begin_ = nullptr;
+        num_ = 0;
+    }
+
+    CircularBuffer(int n) {
+        capacity_ = n;
+        size_ = 0;
+        begin_ = new T[capacity_];
+        num_ = 0;
+    }
+
+    ~CircularBuffer() {
+        delete[] begin_;
+    }
+
+    typedef CircularIterator<CircularBuffer<T>, T> iterator;
+
+    T& operator[](int index) {
+        return *(begin_ + (num_ + index) % capacity_);
+    }
+
+    iterator begin() {
+        return iterator(*this, 0);
+    }
+
+    iterator end() {
+        return iterator(*this, size_);
+    }
+
+    void insertBegin(T element) {
+        if (size_ == capacity_)
+            std::cout << "Buffer is full" << std::endl;
+        else {
+            num_ = (num_ - 1 + capacity_) % capacity_;
+            begin_[num_] = element;
+            size_++;
+        }
+    }
+
+    void deleteBegin() {
+        if (size_ == 0)
+            std::cout << "Buffer is empty" << std::endl;
+        else {
+            size_--;
+            num_ = (num_ + 1 + capacity_) % capacity_;
+        }
+    }
+
+    void insertEnd(T element) {
+        if (size_ == capacity_)
+            std::cout << "Buffer is full" << std::endl;
+        else {
+            int new_num = (num_ + size_ + capacity_) % capacity_;
+            begin_[new_num] = element;
+            size_++;
+        }
+    }
+
+    void deleteEnd() {
+        if (size_ == 0)
+            std::cout << "Buffer is empty" << std::endl;
+        else {
+            size_--;
+        }
+    }
+
+    void insertIt(iterator&& it, T element) {
+        if (it > end())
+            std::cout << "Insert after end!" << std::endl;
+        else {
+            insertEnd(*(end() - 1));
+            iterator temp = end() - 2;
+            while (temp != it) {
+                *temp = *(temp - 1);
+                temp--;
+            }
+            *it = element;
+        }
+    }
+
+    void deleteIt(iterator&& it) {
+        if (it >= end())
+            std::cout << "Delete after end!" << std::endl;
+        else {
+            while (it != end() - 1) {
+                *it = *(it + 1);
+                it++;
+            }
+            deleteEnd();
+        }
+    }
+
+    void changeCap(int value) {
+        if (value < size_)
+            std::cout << "Too little space!" << std::endl;
+        else {
+            T* new_begin = new T[value];
+            int i = num_, j = 0;
+            for (; i < capacity_ && j < size_; i++, j++) {
+                new_begin[j] = begin_[i];
+            }
+            for (i = 0; i < (num_ + size_ - 1 + capacity_) % capacity_ && j < size_; i++, j++) {
+                new_begin[j] = begin_[i];
+            }
+            num_ = 0;
+            capacity_ = value;
+            delete[] begin_;
+            begin_ = new_begin;
+        }
+    }
+};
